9ffc8be92afec1ce8f3f45702a4d8ded5fa6a9b9
RafsanRifat <rafsan.rifat255@gmail.com>
2022-06-02 19:04:03 +0600

    detail view not working

--- Django_Vue_Ecommerce/product/urls.py
+++ Django_Vue_Ecommerce/product/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from .views import LatestProductsList, CustomerList
+from .views import LatestProductsList, CustomerList, ProductDetail
 
 urlpatterns = [
     path('latest-products/', LatestProductsList.as_view(), name='latestProduct'),
     path('customers/', CustomerList.as_view(), name='customer'),
-    path('products/<slug:category_slug>/<slug:product_slug>/', CustomerList.as_view())
+    path('products/<slug:category_slug>/<slug:product_slug>/', ProductDetail.as_view())
 ]
