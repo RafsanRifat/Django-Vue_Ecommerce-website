500176a3a8b83e902ccb5dee47489f524d2adae7
RafsanRifat <rafsan.rifat255@gmail.com>
2022-06-02 13:20:36 +0600

    product.vue added

--- Django_Vue_Ecommerce/product/urls.py
+++ Django_Vue_Ecommerce/product/urls.py
@@ -4,5 +4,6 @@ from .views import LatestProductsList, CustomerList
 
 urlpatterns = [
     path('latest-products/', LatestProductsList.as_view(), name='latestProduct'),
-    path('customers/', CustomerList.as_view(), name='customer')
+    path('customers/', CustomerList.as_view(), name='customer'),
+    path('products/<slug:category_slug>/<slug:product_slug>/', CustomerList.as_view())
 ]
