0a633b7d564e890934d6419b9f05362a63c58521
RafsanRifat <rafsan.rifat255@gmail.com>
2022-06-05 11:24:19 +0600

    vue state setup

--- ecommerce_frontend/src/store/index.js
+++ ecommerce_frontend/src/store/index.js
@@ -1,4 +1,5 @@
 import {createStore} from 'vuex'
+import {parse} from "../../../venv/lib/python3.8/site-packages/rest_framework/static/rest_framework/js/coreapi-0.1.1";
 
 export default createStore({
     state: {
@@ -10,7 +11,22 @@ export default createStore({
         isLoading: false
     },
     getters: {},
-    mutations: {},
+    mutations: {
+        initializeStore(state) {
+            if (localStorage.getItem('cart')) {
+                state.cart = JSON.parse(localStorage.getItem('cart'))
+            } else {
+                localStorage.setItem('cart', JSON.stringify(this.state.cart))
+            }
+        },
+        addToCart(state, item) {
+            const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+
+            if(exists.length) {
+                exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+            }
+        }
+    },
     actions: {},
     modules: {}
 })
