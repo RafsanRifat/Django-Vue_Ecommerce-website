d1510c95b3a2f9c7af2ba0b62c3bb81e0b31ef52
RafsanRifat <rafsan.rifat255@gmail.com>
2022-06-07 18:27:17 +0600

    category page added

--- Django_Vue_Ecommerce/product/urls.py
+++ Django_Vue_Ecommerce/product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import LatestProductsList, CustomerList, ProductDetail
+from .views import LatestProductsList, CustomerList, ProductDetail, CategoryDetail
 
 urlpatterns = [
     path('latest-products/', LatestProductsList.as_view(), name='latestProduct'),
     path('customers/', CustomerList.as_view(), name='customer'),
-    path('products/<slug:category_slug>/<slug:product_slug>/', ProductDetail.as_view())
+    path('products/<slug:category_slug>/<slug:product_slug>/', ProductDetail.as_view()),
+    path('products/<slug:category_slug>/', CategoryDetail.as_view())
 ]
---
+++ ecommerce_frontend/src/views/Category.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page-category">
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Category',
+  data() {
+    return {
+      category: {
+        products: []
+      }
+    }
+  },
+  mounted() {
+    this.getCategory()
+  },
+  methods: {
+    getCategory(){
+      const categorySlug = this.$route.params.category_slug
+      console.log(categorySlug)
+    }
+}
+
+}
+</script>
